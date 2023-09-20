50c7027f0fc07265f9fd0abae6a1d0572abeffdf
nastaj <G00424689@atu.ie>
2023-09-20 19:48:26 +0200

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
@@ -7,8 +9,6 @@ import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Jakub</div>;
+}
+
+export default Username;
